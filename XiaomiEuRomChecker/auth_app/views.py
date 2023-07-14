465cfb7e2e4b9bac4fa483919aee38f585099820
chebishev <atanas.chebishev@gmail.com>
2023-07-14 14:21:45 +0300

    registration form with autologin

--- XiaomiEuRomChecker/auth_app/views.py
+++ XiaomiEuRomChecker/auth_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.contrib.auth import views as auth_views
+from django.contrib.auth import views as auth_views, login
 from django.contrib.auth import forms as auth_forms
+from django.urls import reverse_lazy
 from django.views import generic as views
 
 
@@ -8,6 +9,14 @@ from django.views import generic as views
 class RegisterUserView(views.CreateView):
     template_name = 'auth_app/register.html'
     form_class = auth_forms.UserCreationForm
+    success_url = reverse_lazy('register')
+
+    def form_valid(self, form):
+        result = super().form_valid(form)
+
+        login(self.request, self.object)
+
+        return result
 
 
 class LoginUserView(views.View):
