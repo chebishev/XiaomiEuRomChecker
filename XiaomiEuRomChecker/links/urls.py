d1706b2af5284cd38c6ca6d97823222a67c182e3
chebishev <atanas.chebishev@gmail.com>
2023-07-27 12:43:36 +0300

    testing getting link from previous template in order to be saved in database

--- XiaomiEuRomChecker/links/urls.py
+++ XiaomiEuRomChecker/links/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
-from .views import LinkCreateView, LinkEditView, LinkDeleteView, link_details
+from django.urls import path, include
+from .views import LinkCreateView, LinkEditView, LinkDeleteView, LinkDetailsView
 
 urlpatterns = [
+    path("<int:user_id>/link/", include([
         path("link_add/", LinkCreateView.as_view(), name="link_add"),
-        path('link_details/<slug:slug>/', link_details, name="link_details"),
-        path("link_edit/<slug:slug>/", LinkEditView.as_view(), name="link_edit"),
-        path("link_delete/<slug:slug>/", LinkDeleteView.as_view(), name="link_delete"),
+        path("<slug:slug>/", include([
+            path('link_details/', LinkDetailsView.as_view(), name="link_details"),
+            path("link_edit/", LinkEditView.as_view(), name="link_edit"),
+            path("link_delete/", LinkDeleteView.as_view(), name="link_delete"),
+        ])),
+
+    ]), ),
 ]
--- XiaomiEuRomChecker/links/views.py
+++ XiaomiEuRomChecker/links/views.py
@@ -1,23 +1,44 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
-from django.views.generic import CreateView, UpdateView, DeleteView
+from django.http import request
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
 from XiaomiEuRomChecker.links.models import LinksModel
 
 UserModel = get_user_model()
 
+class LinkDetailsView(LoginRequiredMixin, DetailView):
+    model = LinksModel
+    template_name = 'links/link_details.html'
 
-@login_required
-def link_details(request, slug):
-    link = LinksModel.objects.get(slug=slug)
-    return render(request, 'links/link_details.html', {'link': link})
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['link'] = LinksModel.objects.get(slug=self.kwargs[self.slug_url_kwarg])
+        return context
+
+    def get_success_url(self):
+        return reverse('links:link_details', kwargs={'user_id': self.kwargs['user_id'], 'slug': self.kwargs['slug']})
+
+# TODO To be deleted
+# @login_required
+# def link_details(request, slug):
+#     link = LinksModel.objects.get(slug=slug)
+#     return render(request, 'links/link_details.html', {'link': link})
 
 
 class LinkCreateView(LoginRequiredMixin, CreateView):
     model = LinksModel
     template_name = 'links/link_create.html'
-    fields = ['link_name', 'link_url', 'short_link', 'link_description']
+    fields = ['link_name', 'link_url', 'link_description']
+
+    def dispatch(self, request, *args, **kwargs):
+        test = request.POST.get('save_link')
+        return super().dispatch(request, *args, **kwargs)
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 
 class LinkEditView(LoginRequiredMixin, UpdateView):
