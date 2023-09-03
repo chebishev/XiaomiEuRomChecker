1f62d31d5790fce6ed1d5272aa3e3cc37b3d1039
chebishev <atanas.chebishev@gmail.com>
2023-09-03 22:48:10 +0300

    .env file fixed reading from manage.py

--- XiaomiEuRomChecker/settings.py
+++ XiaomiEuRomChecker/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG') == 'True'
+DEBUG = bool(int(os.getenv('DEBUG')))
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(' ')
 
