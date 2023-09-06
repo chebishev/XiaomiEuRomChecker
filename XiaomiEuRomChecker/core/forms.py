c53cc5d4132c63a55d03c558ce993bf9bf037215
chebishev <atanas.chebishev@gmail.com>
2023-09-06 23:21:43 +0300

    styling

--- XiaomiEuRomChecker/core/forms.py
+++ XiaomiEuRomChecker/core/forms.py
@@ -6,12 +6,13 @@ from django import forms
 class ContactForm(forms.Form):
     code_name = forms.CharField(required=False, max_length=20, label='Code name',
                                 widget=forms.TextInput(
-                                    attrs={'placeholder': 'alioth, zeus, marble', 'autofocus': True}))
+                                    attrs={'placeholder': 'alioth, zeus, marble...', 'autofocus': True}))
     market_name = forms.CharField(required=False, max_length=40, label='Market name',
-                                  widget=forms.TextInput(attrs={'placeholder': 'Poco F3, Xiaomi 13, Mi 10'}))
+                                  widget=forms.TextInput(attrs={'placeholder': 'Poco F3, Xiaomi 13, Mi 10...'}))
     rom_name = forms.CharField(required=False, max_length=30, label='Rom name',
-                               widget=forms.TextInput(attrs={'placeholder': 'XM12, MI10S, FUXI'}))
-    rom_options = forms.ChoiceField(choices=(('stable', 'stable'), ('weekly', 'weekly'), ('both', 'both')), label='Rom options')
+                               widget=forms.TextInput(attrs={'placeholder': 'XM12, MI10S, FUXI...'}))
+    rom_options = forms.ChoiceField(choices=(('stable', 'stable'), ('weekly', 'weekly'), ('both', 'both')),
+                                    label='Rom options')
     email = forms.EmailField(required=False, label='E-mail',
                              widget=forms.TextInput(attrs={'placeholder': 'Fill in for feedback'}))
     message = forms.CharField(required=False, widget=forms.Textarea, label='Optional message')
