778c2afa319e9c000f721f90f3c910510f201b63
chebishev <atanas.chebishev@gmail.com>
2023-12-30 22:24:08 +0200

    test deploy because of an error: http: invalid Host header

--- XiaomiEuRomChecker/settings.py
+++ XiaomiEuRomChecker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.getenv('DEBUG', default=0))
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(' ')
+ALLOWED_HOSTS = ["*"]  # os.getenv('ALLOWED_HOSTS', '').split(' ')
 
 # Application definition
 
