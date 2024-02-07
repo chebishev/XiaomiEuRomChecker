0fa7cb26c6ca2cd92f64bc1636501f9686bbc078
chebishev <atanas.chebishev@gmail.com>
2024-02-07 23:19:43 +0200

    remove missing or unsupported device option

--- XiaomiEuRomChecker/core/views.py
+++ XiaomiEuRomChecker/core/views.py
@@ -1,10 +1,6 @@
-import os
 from django.contrib.auth import get_user_model
-from django.core.mail import send_mail, BadHeaderError
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from XiaomiEuRomChecker import settings
-from XiaomiEuRomChecker.core.forms import ContactForm, get_devices_dict
+
 from XiaomiEuRomChecker.core.functionality import get_link_for_specific_device
 from XiaomiEuRomChecker.core.models import AvailableDevicesModel
 
@@ -55,30 +51,3 @@ def downloads(request, pk, slug):
 # there is a reference to this view in XiaomiEuRomChecker\urls.py
 def page_not_found(request, exception=None):
     return redirect('index')
-
-
-def contact(request):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            subject = "Check device information"
-            body = {
-                'market_name': f"Device market name: {form.cleaned_data['market_name']}",
-                'code_name': f"Device code name: {get_devices_dict()[form.cleaned_data['market_name']]}",
-                'status': f"Report type: {form.cleaned_data['status']}",
-                'rom_options': f"Rom option to check: {form.cleaned_data['rom_options']}",
-                'email': f"Email address: {form.cleaned_data['email']}",
-            }
-            message = "\n".join(body.values())
-            try:
-                send_mail(subject, message, settings.EMAIL_HOST_USER, [os.getenv('EMAIL_RECIPIENT')])
-            except BadHeaderError:
-                return HttpResponse('Invalid header found.')
-            return redirect("thank_you")
-
-    form = ContactForm()
-    return render(request, "core/contact.html", {'form': form})
-
-
-def thank_you(request):
-    return render(request, 'core/thank_you.html')
