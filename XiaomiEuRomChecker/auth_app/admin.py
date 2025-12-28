965293c06ab635e187d784c5e86e61b077ab9a6f
chebishev <atanas.chebishev@gmail.com>
2025-12-28 10:12:33 +0200

    fixed admin.py from ayth_app TODO: Fix my device Resolve by deleting links and my device and device from user

--- XiaomiEuRomChecker/auth_app/admin.py
+++ XiaomiEuRomChecker/auth_app/admin.py
@@ -1,42 +1,48 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.contrib.auth import get_user_model
 
 UserModel = get_user_model()
 
 
-class CustomUserChangeForm(UserChangeForm):
-    class Meta(UserChangeForm.Meta):
-        model = UserModel
-
-
-class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = UserModel
-
-
 @admin.register(UserModel)
 class AuthUserAdmin(UserAdmin):
-    form = CustomUserChangeForm
-    add_form = CustomUserCreationForm
-
-    list_display = ['username', 'last_login', 'is_superuser', 'is_active', 'date_joined']
-    ordering = ['date_joined']
-    filter_fields = ['username', 'last_login', 'is_superuser', 'is_active', 'date_joined']
-    list_filter = ['is_staff']
-    list_editable = ['is_active']
-    # limit user fields in the panel to the following ones:
-    fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Permissions', {'fields': ('is_superuser', 'is_staff', 'is_active', 'groups')}),
+    list_display = (
+        'username',
+        'last_login',
+        'is_superuser',
+        'is_active',
+        'date_joined',
     )
 
-    def save_model(self, request, obj, form, change):
-        if not change:  # Check if this is a new user being created
-            obj.set_password(form.cleaned_data['password1'])  # Use cleaned_data to get the password
-        else:
-            user = UserModel.objects.get(pk=obj.pk)
-            if form.cleaned_data['password'] != user.password:
-                obj.set_password(form.cleaned_data['password'])
-        obj.save()
+    ordering = ('date_joined',)
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    list_editable = ('is_active',)
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Permissions', {
+            'fields': (
+                'is_staff',
+                'is_superuser',
+                'is_active',
+                'groups',
+                'user_permissions',
+            )
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'username',
+                'password1',
+                'password2',
+                'is_staff',
+                'is_superuser',
+                'is_active',
+            ),
+        }),
+    )
