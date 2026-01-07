295ec3c14eb860a42fa4b22dd4e849053988eb35
chebishev <atanas.chebishev@gmail.com>
2026-01-07 15:53:14 +0200

    fix secretkey key

--- XiaomiEuRomChecker/settings.py
+++ XiaomiEuRomChecker/settings.py
@@ -3,13 +3,16 @@ import os
 from pathlib import Path
 
 from django.urls import reverse_lazy
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
