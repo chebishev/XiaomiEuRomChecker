fb22bb4e630abe8d25b6d7e198d74ba9bf55639a
chebishev <atanas.chebishev@gmail.com>
2026-01-07 15:49:45 +0200

    remove pydantic

--- XiaomiEuRomChecker/settings.py
+++ XiaomiEuRomChecker/settings.py
@@ -1,16 +1,18 @@
-import logging
+
 import os
-from functools import lru_cache
 from pathlib import Path
 
 from django.urls import reverse_lazy
-from dotenv import load_dotenv
-from pydantic_settings import BaseSettings, SettingsConfigDict
 
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.getenv('DEBUG')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -119,29 +121,6 @@ LOGIN_URL = reverse_lazy('login')
 
 AUTH_USER_MODEL = 'auth_app.AuthUser'
 
-
-class Settings(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_file=".env", env_ignore_empty=True, extra="ignore"
-    )
-    SECRET_KEY: str
-    DEBUG: bool
-
-
-@lru_cache
-def get_settings() -> Settings:
-    settings = Settings()
-    logging.info(f"SECRET_KEY: {settings.SECRET_KEY}")
-    logging.info(f"DEBUG: {settings.DEBUG}")
-    return settings
-
-
-env_settings = get_settings()
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env_settings.SECRET_KEY
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env_settings.DEBUG
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
